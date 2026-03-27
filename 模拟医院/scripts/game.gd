bf43d300461c93f1f505f7fc4fdd064b0bd7104e
Xander <zhou@MacBook-Air-9.local>
2026-03-27 14:59:02 +0800

    create ICU_stimu

--- 模拟医院/scripts/game.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _ready() -> void:
-	$BackButton.pressed.connect(_on_back_button_pressed)
-
-
-func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
